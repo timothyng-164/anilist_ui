da590d645bf846070139a16f814ced9b35d26653
DESKTOP-LQK5MPG\timot <timothyng164@gmail.com>
2023-11-06 00:59:55 -0800

    add clipboard paste for login

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,6 +1,7 @@
 import 'package:anilist_ui/common/widgets/text_span_link.dart';
 import 'package:anilist_ui/routing/routes.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:provider/provider.dart';
 
@@ -52,6 +53,7 @@ class PageContent extends HookWidget {
     final authToken = useState<String>('');
     final isCheckingAuth = useState<bool>(false);
     final errorText = useState<String?>(null);
+    final textController = useTextEditingController();
 
     var authState = context.watch<AuthState>();
 
@@ -119,6 +121,7 @@ class PageContent extends HookWidget {
         ),
         const SizedBox(height: 20),
         TextField(
+          controller: textController,
           onChanged: (value) => authToken.value = value,
           onSubmitted: (value) async {
             authToken.value = value;
@@ -128,6 +131,14 @@ class PageContent extends HookWidget {
             border: const OutlineInputBorder(),
             hintText: 'Paste text',
             errorText: errorText.value,
+            suffixIcon: IconButton(
+                onPressed: () async {
+                  var clip = await Clipboard.getData('text/plain');
+                  String text = clip?.text?.trim() ?? '';
+                  textController.text = text;
+                  authToken.value = text;
+                },
+                icon: const Icon(Icons.paste_rounded)),
           ),
         ),
         const SizedBox(height: 30),
